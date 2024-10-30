e8e0b52bcc5e159cef602b387605e81bbc0ad198
TheTechromancer <thetechromancer@protonmail.com>
2024-10-30 15:22:41 -0400

    fix bug

--- radixtarget/__init__.py
+++ radixtarget/__init__.py
@@ -1,5 +1,5 @@
 from .radixtarget import RadixTarget
-from .radixtarget.helpers import host_size_key  # noqa
+from .helpers import host_size_key  # noqa
 
 # alias for convenience
 Target = RadixTarget
